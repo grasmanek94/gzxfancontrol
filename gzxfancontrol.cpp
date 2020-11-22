7c90b82d3f7d045334da6f8784a3165ef5a032f7
Gamer_Z <github@gz0.nl>
2020-11-22 17:04:23 +0100

    This one is more silent than the old one

--- gzxfancontrol.cpp
+++ gzxfancontrol.cpp
@@ -1,66 +1,116 @@
-#include <cstdio>
+#include <algorithm>
 #include <cstdlib>
-#include <unistd.h>
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <thread>
 
-float lerp(float a, float b, float f)
+const float C_TEMP_MIN = 45.0f;
+const float C_TEMP_MAX = 85.0f;
+const float C_TEMP_RANGE = C_TEMP_MAX - C_TEMP_MIN;
+
+const int C_SPEED_MIN = 0x00;
+const int C_SPEED_MAX = 0x64;
+const int C_SPEED_RANGE = C_SPEED_MAX - C_SPEED_MIN;
+
+const float C_SPEED_PER_DEGREE = (float)C_SPEED_RANGE / C_TEMP_RANGE;
+const float C_DEGREE_PER_SPEED = C_TEMP_RANGE / (float)C_SPEED_RANGE;
+
+float GetTemp()
 {
-	return a + f * (b - a);
+	static const size_t max_zones = 2;
+	static const std::string filenames[max_zones]{
+		"/sys/class/thermal/thermal_zone0/temp",
+		"/sys/class/thermal/thermal_zone1/temp"
+	};
+
+	static std::fstream temperature_file[max_zones]{
+		std::fstream(filenames[0], std::ios_base::in),
+		std::fstream(filenames[1], std::ios_base::in)
+	};
+
+	static float temperature[max_zones]{ C_TEMP_MAX, C_TEMP_MAX };
+
+	float max_temp = C_TEMP_MIN;
+
+	for (size_t i = 0; i < max_zones; ++i)
+	{
+		if (!temperature_file[i])
+		{
+			temperature_file[i].close();
+			temperature_file[i].open(filenames[i], std::ios_base::in);
+			return C_TEMP_MAX;
+		}
+
+		temperature_file[i].seekg(0, temperature_file[i].beg);
+		if (!(temperature_file[i] >> temperature[i]))
+		{
+			temperature_file[i].close();
+			temperature_file[i].open(filenames[i], std::ios_base::in);
+			return C_TEMP_MAX;
+		}
+
+		temperature[i] /= 1000.0f;
+		if (max_temp < temperature[i])
+		{
+			max_temp = temperature[i];
+		}
+	}
+
+	return std::min(std::max(max_temp, C_TEMP_MIN), C_TEMP_MAX);
 }
 
-int GetFanSpeed(float min_temp, float max_temp, float current_temp_a, float current_temp_b)
+void exec(const char* cmd)
 {
-	float current_temp = current_temp_a < current_temp_b ? current_temp_b : current_temp_a;
-	float diff_temp = max_temp - min_temp;
-	float current_progress = current_temp < min_temp ? 0.0f : (current_temp - min_temp);
-	float output_speed = lerp(0.0f, 255.0f, current_progress / diff_temp);
-	return (int)output_speed;
+	FILE* process = popen(cmd, "r");
+	if (process)
+	{
+		pclose(process);
+	}
+}
+
+void SetFanSpeed(int speed)
+{
+	speed = std::min(std::max(speed, C_SPEED_MIN), C_SPEED_MAX);
+
+	std::stringstream stream;
+
+	stream << "ipmitool raw 0x30 0x70 0x66 0x01 0x00 0x";
+
+	if (speed <= 0x0F)
+	{
+		stream << "0";
+	}
+
+	stream << std::hex << speed;
+
+	exec(stream.str().c_str());
+}
+
+int CalculateSpeed(float temp)
+{
+	float temp_offset = temp - C_TEMP_MIN;
+	return (int)(temp_offset * C_SPEED_PER_DEGREE);
 }
 
 int main()
 {
-	float MIN_TEMP = 42000.0f;
-	float MAX_TEMP = 82000.0f;
-
-	char buffer[128];
-
-	FILE *temp_a;
-	FILE *temp_b;
-
-	temp_a = fopen("/sys/class/thermal/thermal_zone0/temp", "r");
-	temp_b = fopen("/sys/class/thermal/thermal_zone1/temp", "r");
-
-	float r_temp_a = 0.00f;
-	float r_temp_b = 0.00f;
-
-	int fan_speed = 0;
-
-	while (1)
+	while (true)
 	{
-		fseek(temp_a, 0, SEEK_SET);
-		fseek(temp_b, 0, SEEK_SET);
+		auto sleep_until = std::chrono::steady_clock::now() + std::chrono::milliseconds(250);
 
-		fread(buffer, 128, 1, temp_a);
-		r_temp_a = (float)atoi(buffer);
+		float temp = GetTemp();
+		int speed = CalculateSpeed(temp);
+		SetFanSpeed(speed);
 
-		fread(buffer, 128, 1, temp_b);
-		r_temp_b = (float)atoi(buffer);
+#ifndef NDEBUG
+		std::cout << GetTemp() << " / " << speed << std::endl;
+#endif
 
-		fan_speed = GetFanSpeed(MIN_TEMP, MAX_TEMP, r_temp_a, r_temp_b);
-		if(fan_speed > 255)
-		{
-			fan_speed = 255;
-		}
-		sprintf(buffer, "ipmitool raw 0x30 0x70 0x66 0x01 0x00 0x%02x", fan_speed);
-
-		system(buffer);
-
-		printf("%f %f\n", r_temp_a, r_temp_b);
-
-		usleep(1);
+		std::this_thread::sleep_until(sleep_until);
 	}
-
-	fclose(temp_a);
-	fclose(temp_b);
 
 	return 0;
 }
